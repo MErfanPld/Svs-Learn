39c191acf41e40957424c03234a124a97d60653d
MErfanPld <m.erfanpouladzadeh@yahoo.com>
2025-06-01 19:15:56 -0400

    Edit ...

--- courses/helpers.py
+++ courses/helpers.py
@@ -2,7 +2,7 @@ import time
 from django.utils.text import slugify
 
 def upload_courses_category_image(instance, filename):
-    path = 'uploads/' + 'category/' + 'courses'
-    name = str(time.time()) + '-' + str(instance.name) + '-' + filename
-    return path + '/' + name
-
+    path = 'uploads/category/courses/' + slugify(instance.name, allow_unicode=True)
+    safe_name = slugify(instance.name, allow_unicode=True)
+    name = f"{int(time.time())}-{safe_name}-{filename}"
+    return f"{path}/{name}"
